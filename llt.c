38271d265d608163209e65954b22741551a01770
0x6775737461 <fabiogustavo@tuta.io>
2021-04-07 19:32:30 -0300

    checking invalid inputs and adding params support

--- llt.c
+++ llt.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static void usage(void);
+static int param_checker(const char *);
 
 double farenheit(double);
 
@@ -7,17 +11,34 @@ int main(int argc, char *argv[]){
 	char *check = NULL;
 	double n = 0;
 
-	if(argc != 2)
-		return 1;
-	
-	n = strtod(argv[1], &check);
+	if((argc != 3) || !param_checker(argv[1])) {
+		usage();
+		return EXIT_FAILURE;
+	}
+		
+	n = strtod(argv[2], &check);
+	if(*check) {
+		fprintf(stderr,
+						"\b[ERROR] Invalid Value: %s!\n", argv[2]);
+		return EXIT_FAILURE;
+	}
 
-	if(check)
-		printf("[%10.2f ºC]\t[%10.2f ºF]\n", n, farenheit(n));
-	else
-		return 1;
+	printf("[%10.2f ºC]\t[%10.2f ºF]\n", n, farenheit(n));
 	
 	return 0;
 }
 
-double farenheit(double celsius) { return ((9.0/5.0) * celsius + 32); }
+void usage(void) {
+	fprintf(stderr,
+					"\bUsage:\n"
+					"\b\t#Converting Celsius to Farenheit\n"
+					"\b\t$ ./llt -cf [0-9]\n");
+}
+
+int param_checker(const char *param) {
+	//TODO: think of another way to check mult params..
+	return !strcmp(param, "-cf");
+}
+
+//Converters
+double farenheit(double celsius) { return (9 / 5 * celsius + 32); }
